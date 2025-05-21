06f4138b2614f6bf729827514d9cabced23dcbf2
CezarEngenheiroDeSoftware <cezar.audelania@gmail.com>
2025-05-21 13:17:22 -0300

    Criando a tabela de Usuários

--- Prédio Comercial/Models/Usuarios.cs
+++ Prédio Comercial/Models/Usuarios.cs
@@ -7,9 +7,14 @@ namespace Prédio_Comercial.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MinLength(8)]
+        [StringLength(100)]
         public string Login { get; set; } = "";
         [Required]
+        [MinLength(5)]
+        [MaxLength(100)]
         public string Password { get; set; } = "";
-        public bool Admin { get; set; } = false;
+        public DateTime? DataContratacao { get; set; } = DateTime.Now;
+        public bool? Admin { get; set; } = false;
     }
 }
